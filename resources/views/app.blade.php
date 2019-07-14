8389ea5082edb670d241d2dac8ceb3c551202970
708::U <arter89@gmail.com>
2019-07-14 23:33:51 +0900

    Add css asset to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>@yield('title') | Laravel Tutorial Sample App</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="all">
     </head>
     <body>
         <h1>Sample App</h1>
